2bd387e4bad0285434a746c02f8e4ec4a0994234
Daniell Algar <daniell.algar@varvet.com>
2016-08-29 09:16:41 +0200

    CardView onClick finally works

--- app/src/main/java/com/algar/ifuckforwind/activity/DetailCardActivity.java
+++ app/src/main/java/com/algar/ifuckforwind/activity/DetailCardActivity.java
@@ -4,47 +4,47 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.algar.ifuckforwind.R;
 import com.algar.ifuckforwind.adapter.DetailActivityAdapter;
 import com.algar.ifuckforwind.fragment.SectionsPagerFragment;
 
-public class DetailCardActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailCardActivity extends AppCompatActivity implements DetailActivityAdapter.Caller {
 
     private boolean mIsHappyDay;
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    @BindView(R.id.detail_recycler_view) RecyclerView mRecyclerView;
+    private DetailActivityAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_card);
+        ButterKnife.bind(this);
         //noinspection ConstantConditions
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle(getIntent().getStringExtra(SectionsPagerFragment.INTENT_EXTRA_CURRENT_DAY));
+        mRecyclerView.setHasFixedSize(true);
 
         mIsHappyDay = getIntent().getBooleanExtra(SectionsPagerFragment.INTENT_EXTRA_IS_HAPPY_DAY, true);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.detail_recycler_view);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        // specify an adapter (see also next example)
-        mAdapter = new DetailActivityAdapter(
+        mAdapter = new DetailActivityAdapter(this,
                 new int[]{R.drawable.getskar, R.drawable.apelviken},
                 new String[]{"Getskär", "Apelviken"},
                 new String[]{"41 km", "79 km"},
                 new String[]{"Midday: 8-10 m/s", "Midday: 7-13 m/s"},
                 new String[]{"Afternoon: 9-13 m/s", "Afternoon: 8-13 m/s"});
+
         mRecyclerView.setAdapter(mAdapter);
     }
 
@@ -55,5 +55,10 @@ public class DetailCardActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClicked(View view) {
+        Log.v("tag", "snyggTag");
     }
 }
--- app/src/main/java/com/algar/ifuckforwind/adapter/DetailActivityAdapter.java
+++ app/src/main/java/com/algar/ifuckforwind/adapter/DetailActivityAdapter.java
@@ -1,10 +1,12 @@
 package com.algar.ifuckforwind.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.algar.ifuckforwind.R;
@@ -19,30 +21,16 @@ public class DetailActivityAdapter extends RecyclerView.Adapter<DetailActivityAd
     private String[] mSpotWindDay;
     private String[] mSpotWindAfternoon;
     private int[] mSpotAvatar;
+    private LinearLayout mCardViewContainer;
+    private Caller mListener;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public ImageView mSpotAvatar_imageview;
-        public TextView mSpotName_textview;
-        public TextView mSpotDistance_textview;
-        public TextView mWindStrengthDay_textview;
-        public TextView mWindStrengtAfternoon_textview;
-
-        public ViewHolder(View v) {
-            super(v);
-            mSpotAvatar_imageview = (ImageView) v.findViewById(R.id.detail_cardview_spot_avatar_imageview);
-            mSpotName_textview = (TextView) v.findViewById(R.id.detail_cardview_spot_name_textview);
-            mSpotDistance_textview = (TextView) v.findViewById(R.id.detail_cardview_spot_distance_textview);
-            mWindStrengthDay_textview = (TextView) v.findViewById(R.id.detail_cardview_wind_day_textview);
-            mWindStrengtAfternoon_textview = (TextView) v.findViewById(R.id.detail_cardview_wind_afternoon_textview);
-        }
+    public interface Caller {
+        void onItemClicked(View view);
     }
 
-    public DetailActivityAdapter(int[] spotAvatar, String[] spotNames, String[] spotDistance,
+    public DetailActivityAdapter(Caller listener, int[] spotAvatar, String[] spotNames, String[] spotDistance,
                                  String[] spotWindDay, String[] spotWindAfternoon) {
+        mListener = listener;
         mSpotNames = spotNames;
         mSpotDistance = spotDistance;
         mSpotWindDay = spotWindDay;
@@ -51,15 +39,20 @@ public class DetailActivityAdapter extends RecyclerView.Adapter<DetailActivityAd
     }
 
     @Override
-    public DetailActivityAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
-        View v = LayoutInflater.from(parent.getContext())
+    public ViewHolder onCreateViewHolder(ViewGroup parent, final int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.cardview, parent, false);
         // set the view's size, margins, paddings and layout parameters
 
-        ViewHolder vh = new ViewHolder(v);
+        ViewHolder vh = new ViewHolder(itemView, new ViewHolder.ViewHolderClicks() {
+            @Override
+            public void itemClicked(View view) {
+                mListener.onItemClicked(view);
+            }
+        });
         return vh;
     }
+
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
@@ -73,5 +66,35 @@ public class DetailActivityAdapter extends RecyclerView.Adapter<DetailActivityAd
     @Override
     public int getItemCount() {
         return mSpotNames.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        // each data item is just a string in this case
+        public ImageView mSpotAvatar_imageview;
+        public TextView mSpotName_textview;
+        public TextView mSpotDistance_textview;
+        public TextView mWindStrengthDay_textview;
+        public TextView mWindStrengtAfternoon_textview;
+        public ViewHolderClicks mListener;
+
+        public ViewHolder(View itemView, ViewHolderClicks listener) {
+            super(itemView);
+            mListener = listener;
+            mSpotAvatar_imageview = (ImageView) itemView.findViewById(R.id.detail_cardview_spot_avatar_imageview);
+            mSpotName_textview = (TextView) itemView.findViewById(R.id.detail_cardview_spot_name_textview);
+            mSpotDistance_textview = (TextView) itemView.findViewById(R.id.detail_cardview_spot_distance_textview);
+            mWindStrengthDay_textview = (TextView) itemView.findViewById(R.id.detail_cardview_wind_day_textview);
+            mWindStrengtAfternoon_textview = (TextView) itemView.findViewById(R.id.detail_cardview_wind_afternoon_textview);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            mListener.itemClicked(view);
+        }
+
+        interface ViewHolderClicks {
+            void itemClicked(View caller);
+        }
     }
 }
