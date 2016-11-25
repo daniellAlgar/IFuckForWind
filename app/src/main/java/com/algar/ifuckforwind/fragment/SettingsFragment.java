54a96a95c24df04717b42df4270ea12ea59718c8
Daniell Algar <daniell.algar@varvet.com>
2016-11-25 23:37:22 +0100

    Fix PreferenceFragment in ViewPager.

--- app/src/main/java/com/algar/ifuckforwind/fragment/SettingsFragment.java
+++ app/src/main/java/com/algar/ifuckforwind/fragment/SettingsFragment.java
@@ -2,17 +2,17 @@ package com.algar.ifuckforwind.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.algar.ifuckforwind.R;
+import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
 
 /**
- * Created by algar on 2016-08-30.
+ * Created by algar on 2016-08-30
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     private static final String SECTION_NUMBER = "sectionNumber";
     private int mSectionNumber;
@@ -27,19 +27,21 @@ public class SettingsFragment extends Fragment {
         return settingsFragment;
     }
 
+
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreatePreferencesFix(@Nullable Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preference, rootKey);
         mSectionNumber = getArguments().getInt(SECTION_NUMBER);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
+        return super.onCreateView(inflater, container, savedInstanceState);
 
-        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
-
-        return rootView;
+//        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+//
+//        return rootView;
     }
 }
