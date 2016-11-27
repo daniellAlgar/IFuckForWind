08f8aa164a6b42d182438fe99820356f7f080455
Daniell Algar <daniell.algar@varvet.com>
2016-11-27 22:12:00 +0100

    Couldn’t use mCacheSize for unknown reason. Take another look at this in future.

--- app/src/main/java/com/algar/ifuckforwind/util/LRUCache.java
+++ app/src/main/java/com/algar/ifuckforwind/util/LRUCache.java
@@ -9,14 +9,14 @@ import android.support.v4.util.LruCache;
 public class LRUCache {
     private static LRUCache ourInstance = new LRUCache();
     private LruCache<Object, Object> lru;
-    private static int cacheSize = 1024 * 1024;        // 1 MiB
+    private static int mCacheSize = 1024 * 1024;        // 1 MiB
 
     public static LRUCache getInstance() {
         return ourInstance;
     }
 
     private LRUCache() {
-        lru = new LruCache<>(1024);
+        lru = new LruCache<>(1024 * 1024);
     }
 
     public LruCache<Object, Object> getLru() {
