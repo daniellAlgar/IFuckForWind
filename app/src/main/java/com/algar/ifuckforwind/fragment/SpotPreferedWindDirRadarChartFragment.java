1eed8806833608713c3db5c5237706a205af5865
Daniell Algar <daniell.algar@varvet.com>
2016-12-02 21:32:09 +0100

    First working draft of radar plot for spot prefered wind dir.

---
+++ app/src/main/java/com/algar/ifuckforwind/fragment/SpotPreferedWindDirRadarChartFragment.java
@@ -0,0 +1,189 @@
+package com.algar.ifuckforwind.fragment;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.algar.ifuckforwind.R;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
+import com.github.mikephil.charting.utils.MPPointF;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+/**
+ * Created by algar on 2016-12-02
+ */
+
+public class SpotPreferedWindDirRadarChartFragment extends Fragment {
+
+    private RadarChart mChart;
+    Typeface mTfLight;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View rootView = inflater.inflate(R.layout.fragment_spot_pref_wind_dir_radar_chart, container, false);
+        mChart = (RadarChart) rootView.findViewById(R.id.radar_chart);
+        mChart.setBackgroundColor(Color.rgb(60, 65, 82));
+
+        mChart.getDescription().setEnabled(false);
+
+        mChart.setWebLineWidth(1f);
+        mChart.setWebColor(Color.LTGRAY);
+        mChart.setWebLineWidthInner(1f);
+        mChart.setWebColorInner(Color.LTGRAY);
+        mChart.setWebAlpha(100);
+
+        // create a custom MarkerView (extend MarkerView) and specify the layout
+        // to use for it
+        MarkerView mv = new RadarMarkerView(getActivity().getApplicationContext(), R.layout.radar_markerview);
+        mv.setChartView(mChart); // For bounds control
+        mChart.setMarker(mv); // Set the marker to the chart
+
+        setData();
+
+        mChart.animateXY(
+                1400, 1400,
+                Easing.EasingOption.EaseInOutQuad,
+                Easing.EasingOption.EaseInOutQuad);
+
+        mTfLight = Typeface.createFromAsset(getActivity().getAssets(), "OpenSans-Light.ttf");;
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setTypeface(mTfLight);
+        xAxis.setTextSize(9f);
+        xAxis.setYOffset(0f);
+        xAxis.setXOffset(0f);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+
+            private String[] mActivities = new String[]{"North", "NE", "East", "SE", "South",
+                                                        "SW", "West", "NW"};
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return mActivities[(int) value % mActivities.length];
+            }
+        });
+        xAxis.setTextColor(Color.WHITE);
+
+        YAxis yAxis = mChart.getYAxis();
+        yAxis.setTypeface(mTfLight);
+        yAxis.setLabelCount(5, false);
+        yAxis.setTextSize(9f);
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(80f);
+        yAxis.setDrawLabels(false);
+
+        Legend l = mChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setTypeface(mTfLight);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(5f);
+        l.setTextColor(Color.WHITE);
+
+        return rootView;
+    }
+
+    public void setData() {
+        float mult = 80;
+        float min = 20;
+        int cnt = 8;
+
+        ArrayList<RadarEntry> entries1 = new ArrayList<RadarEntry>();
+//        ArrayList<RadarEntry> entries2 = new ArrayList<RadarEntry>();
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (int i = 0; i < cnt; i++) {
+            float val1 = (float) (Math.random() * mult) + min;
+            entries1.add(new RadarEntry(val1));
+
+//            float val2 = (float) (Math.random() * mult) + min;
+//            entries2.add(new RadarEntry(val2));
+        }
+
+        RadarDataSet set1 = new RadarDataSet(entries1, "Last Week");
+        set1.setColor(Color.rgb(103, 110, 129));
+        set1.setFillColor(Color.rgb(103, 110, 129));
+        set1.setDrawFilled(true);
+        set1.setFillAlpha(180);
+        set1.setLineWidth(2f);
+        set1.setDrawHighlightCircleEnabled(true);
+        set1.setDrawHighlightIndicators(false);
+
+//        RadarDataSet set2 = new RadarDataSet(entries2, "This Week");
+//        set2.setColor(Color.rgb(121, 162, 175));
+//        set2.setFillColor(Color.rgb(121, 162, 175));
+//        set2.setDrawFilled(true);
+//        set2.setFillAlpha(180);
+//        set2.setLineWidth(2f);
+//        set2.setDrawHighlightCircleEnabled(true);
+//        set2.setDrawHighlightIndicators(false);
+
+        ArrayList<IRadarDataSet> sets = new ArrayList<IRadarDataSet>();
+        sets.add(set1);
+//        sets.add(set2);
+
+        RadarData data = new RadarData(sets);
+        data.setValueTypeface(mTfLight);
+        data.setValueTextSize(8f);
+        data.setDrawValues(false);
+        data.setValueTextColor(Color.WHITE);
+
+        mChart.setData(data);
+        mChart.invalidate();
+    }
+
+    public static class RadarMarkerView extends MarkerView {
+
+        private TextView tvContent;
+        private DecimalFormat format = new DecimalFormat("##0");
+
+        public RadarMarkerView(Context context, int layoutResource) {
+            super(context, layoutResource);
+
+            tvContent = (TextView) findViewById(R.id.tvContent);
+            tvContent.setTypeface(Typeface.createFromAsset(context.getAssets(), "OpenSans-Light.ttf"));
+        }
+
+        // callbacks everytime the MarkerView is redrawn, can be used to update the
+        // content (user-interface)
+        @Override
+        public void refreshContent(Entry e, Highlight highlight) {
+            tvContent.setText(format.format(e.getY()) + " %");
+
+            super.refreshContent(e, highlight);
+        }
+
+        @Override
+        public MPPointF getOffset() {
+            return new MPPointF(-(getWidth() / 2), -getHeight() - 10);
+        }
+    }
+}
---
+++ app/src/main/java/com/algar/ifuckforwind/fragment/WindChartFragment.java
@@ -0,0 +1,319 @@
+package com.algar.ifuckforwind.fragment;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.algar.ifuckforwind.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.listener.BubbleChartOnValueSelectListener;
+import lecho.lib.hellocharts.listener.ViewportChangeListener;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.BubbleChartData;
+import lecho.lib.hellocharts.model.BubbleValue;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.BubbleChartView;
+import lecho.lib.hellocharts.view.ColumnChartView;
+import lecho.lib.hellocharts.view.PreviewColumnChartView;
+
+/**
+ * Created by algar on 2016-12-02
+ */
+
+public class WindChartFragment extends Fragment {
+    // TODO: Preview fönstret ska matcha färgen från action bar bakgrounden
+
+    private boolean mHasLabels = true;
+    private boolean mHasLabelForSelected = true;
+    private boolean mHasAxes = true;
+    private boolean mHasAxesNames = true;
+
+    private ColumnChartView mColumnChartView;
+    private PreviewColumnChartView mPreviewChart;
+    private ColumnChartData mColumnChartData;
+    private BubbleChartView mBubbleChartView;
+    private BubbleChartData mBubbleChartData;
+    private ValueShape mBubbleShape = ValueShape.CIRCLE;
+
+    private int mNumYValues = 24;
+    private Bitmap mArrowBitmap;
+
+    /**
+     * Deep copy of mColumnChartData.
+     */
+    private ColumnChartData mPreviewData;
+
+    public WindChartFragment() {
+        // Default empty constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        View rootView = inflater.inflate(R.layout.fragment_preview_column_chart, container, false);
+
+        mColumnChartView = (ColumnChartView) rootView.findViewById(R.id.wind_column_chart_view);
+        mPreviewChart = (PreviewColumnChartView) rootView.findViewById(R.id.chart_preview);
+        mBubbleChartView = (BubbleChartView) rootView.findViewById(R.id.wind_direction_bubble_chart_view);
+        mBubbleChartView.setOnValueTouchListener(new ValueTouchListener());
+
+        // Generate mColumnChartData for previewed mColumnChartView and copy of that mColumnChartData for preview mColumnChartView.
+        generateStackedData();
+
+        mColumnChartView.setColumnChartData(mColumnChartData);
+        // Disable zoom/scroll for previewed mColumnChartView, visible mColumnChartView ranges depends on preview mColumnChartView viewport so
+        // zoom/scroll is unnecessary.
+        mColumnChartView.setZoomEnabled(false);
+        mColumnChartView.setScrollEnabled(false);
+
+        mPreviewChart.setColumnChartData(mPreviewData);
+        mPreviewChart.setViewportChangeListener(new ViewportListener());
+        previewX(true);
+
+        // Generate mBubbleChartData
+        setupArrows();
+        generateBubbleChartArrowData();
+        mBubbleChartView.setBubbleChartData(mBubbleChartData);
+
+        return rootView;
+    }
+
+    // MENU
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.preview_column_chart, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_reset) {
+            generateStackedData();
+            mColumnChartView.setColumnChartData(mColumnChartData);
+            mPreviewChart.setColumnChartData(mPreviewData);
+            previewX(true);
+            return true;
+        }
+        if (id == R.id.action_preview_both) {
+            previewXY();
+            mPreviewChart.setZoomType(ZoomType.HORIZONTAL_AND_VERTICAL);
+            return true;
+        }
+        if (id == R.id.action_preview_horizontal) {
+            previewX(true);
+            return true;
+        }
+        if (id == R.id.action_preview_vertical) {
+            previewY();
+            return true;
+        }
+        if (id == R.id.action_change_color) {
+            int color = ChartUtils.pickColor();
+            while (color == mPreviewChart.getPreviewColor()) {
+                color = ChartUtils.pickColor();
+            }
+            mPreviewChart.setPreviewColor(color);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth)
+    {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // create a matrix for the manipulation
+        Matrix matrix = new Matrix();
+        // resize the bit map
+        matrix.postScale(scaleWidth, scaleHeight);
+        // recreate the new Bitmap
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+        return resizedBitmap;
+    }
+
+    public int pxToDp(int px) {
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        int dp = Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        return dp;
+    }
+
+    public static Bitmap rotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
+    private void setupArrows() {
+        int scaleFactor = 8;
+
+        mArrowBitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.wind_dir_arrow);
+        mArrowBitmap = getResizedBitmap(mArrowBitmap,
+                mArrowBitmap.getWidth()/scaleFactor,
+                mArrowBitmap.getHeight()/scaleFactor);
+    }
+
+    private void generateBubbleChartArrowData() {
+
+        List<BubbleValue> values = new ArrayList<>();
+        for (int i = 0; i < mNumYValues; ++i) {
+            BubbleValue value = new BubbleValue(i, 1, 1);
+            value.setShape(ValueShape.ARROW);
+            value.setBitmap(rotateBitmap(mArrowBitmap, (float) Math.random() * 100));
+            values.add(value);
+        }
+
+        mBubbleChartData = new BubbleChartData(values);
+        mBubbleChartData.setHasLabels(mHasAxes);
+        mBubbleChartData.setHasLabelsOnlyForSelected(mHasLabelForSelected);
+
+        if (mHasLabels) {
+            Axis axisY = new Axis().setHasLines(false);
+            if (mHasAxesNames) {
+                axisY.setName(" ");
+            }
+            mBubbleChartData.setAxisXBottom(null);
+            mBubbleChartData.setAxisYLeft(axisY);
+        } else {
+            mBubbleChartData.setAxisXBottom(null);
+            mBubbleChartData.setAxisYLeft(null);
+        }
+
+        mBubbleChartView.setBubbleChartData(mBubbleChartData);
+        mBubbleChartView.setScrollEnabled(false);
+    }
+
+    private void generateStackedData() {
+        int numSubcolumns = 2;
+        // Column can have many stacked sub-columns, here I use 4 stacked sub-column in each of 4 columns.
+        List<Column> columns = new ArrayList<Column>();
+        List<SubcolumnValue> subcolumnValues;
+        for (int i = 0; i < mNumYValues; ++i) {
+
+            subcolumnValues = new ArrayList<SubcolumnValue>();
+            for (int j = 0; j < numSubcolumns; ++j) {
+                subcolumnValues.add(new SubcolumnValue((float) Math.random() * 20f + 5, ChartUtils.pickColor()));
+            }
+
+            Column column = new Column(subcolumnValues);
+            column.setHasLabels(mHasLabels);
+            column.setHasLabelsOnlyForSelected(mHasLabelForSelected);
+            columns.add(column);
+        }
+
+        mColumnChartData = new ColumnChartData(columns);
+
+        // Set stacked flag.
+        mColumnChartData.setStacked(true);
+
+        // prepare preview mColumnChartData, is better to use separate deep copy for preview mColumnChartView.
+        // set color to grey to make preview area more visible.
+        mPreviewData = new ColumnChartData(mColumnChartData);
+        for (Column column : mPreviewData.getColumns()) {
+            for (SubcolumnValue value : column.getValues()) {
+                value.setColor(ChartUtils.DEFAULT_DARKEN_COLOR);
+            }
+        }
+
+        List<AxisValue> axisValues = new ArrayList<>();
+        for (int i = 0; i < mNumYValues; i++) {
+            axisValues.add(new AxisValue(i));
+        }
+
+        if (mHasAxes) {
+            Axis axisX = new Axis().setValues(axisValues);
+            Axis axisY = new Axis().setHasLines(true);
+            if (mHasAxesNames) {
+                axisX.setName("Time (h)");
+                axisY.setName("m/s");
+            }
+            mColumnChartData.setAxisXBottom(axisX);
+            mColumnChartData.setAxisYLeft(axisY);
+        } else {
+            mColumnChartData.setAxisXBottom(null);
+            mColumnChartData.setAxisYLeft(null);
+        }
+
+        mColumnChartView.setColumnChartData(mColumnChartData);
+    }
+
+    private void previewY() {
+        Viewport tempViewport = new Viewport(mColumnChartView.getMaximumViewport());
+        float dy = tempViewport.height() / 4;
+        tempViewport.inset(0, dy);
+        mPreviewChart.setCurrentViewportWithAnimation(tempViewport);
+        mPreviewChart.setZoomType(ZoomType.VERTICAL);
+    }
+
+    private void previewX(boolean animate) {
+        Viewport tempViewport = new Viewport(mColumnChartView.getMaximumViewport());
+        float dx = tempViewport.width() / 4;
+        tempViewport.inset(dx, 0);
+        if (animate) {
+            mPreviewChart.setCurrentViewportWithAnimation(tempViewport);
+        } else {
+            mPreviewChart.setCurrentViewport(tempViewport);
+        }
+        mPreviewChart.setZoomType(ZoomType.HORIZONTAL);
+    }
+
+    private void previewXY() {
+        // Better to not modify viewport of any mColumnChartView directly so create a copy.
+        Viewport tempViewport = new Viewport(mColumnChartView.getMaximumViewport());
+        // Make temp viewport smaller.
+        float dx = tempViewport.width() / 4;
+        float dy = tempViewport.height() / 4;
+        tempViewport.inset(dx, dy);
+        mPreviewChart.setCurrentViewportWithAnimation(tempViewport);
+    }
+
+    /**
+     * Viewport listener for preview mColumnChartView(lower one). in {@link #onViewportChanged(Viewport)} method change
+     * viewport of upper mColumnChartView.
+     */
+    private class ViewportListener implements ViewportChangeListener {
+
+        @Override
+        public void onViewportChanged(Viewport newViewport) {
+            // don't use animation, it is unnecessary when using preview mColumnChartView because usually viewport changes
+            // happens to often.
+            mColumnChartView.setCurrentViewport(newViewport);
+            mBubbleChartView.setCurrentViewport(newViewport);
+        }
+    }
+
+    private class ValueTouchListener implements BubbleChartOnValueSelectListener {
+
+        @Override
+        public void onValueSelected(int bubbleIndex, BubbleValue value) {
+            Toast.makeText(getActivity(), "Selected: " + value, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onValueDeselected() {
+            // TODO Auto-generated method stub
+        }
+    }
+}
